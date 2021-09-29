b483f1e3be9dc9db655dbb3e728bccdeadb08327
Zita <ambrus.zita99@gmail.com>
2021-09-29 15:13:21 +0200

    Osztály,alap metódusok létrehozása

---
+++ src/Bejegyzes.java
@@ -0,0 +1,39 @@
+import java.time.LocalDateTime;
+
+public class Bejegyzes {
+    private String szerzo;
+    private String tartalom;
+    private int likeok=0;
+    private LocalDateTime letrejott=LocalDateTime.now();
+    private LocalDateTime szerkesztve=LocalDateTime.now();
+
+
+    public Bejegyzes(String szerzo, String tartalom){
+        this.szerzo= szerzo;
+        this.tartalom=tartalom;
+    }
+    public String getSzerzo(){
+        return this.szerzo;
+    }
+    public String getTartalom(){
+        szerkesztve=LocalDateTime.now();
+        return this.tartalom;
+    }
+    public void setTartalom(String tartalom){
+
+    }
+    public int getLikeok(){
+        return this.likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return this.letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return szerkesztve;
+    }
+    public void like(){
+        this.likeok++;
+    }
+}
