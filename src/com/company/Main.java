67762c34e673d626bbba31fa97601243a79c739e
Zita <ambrus.zita99@gmail.com>
2021-09-29 16:27:14 +0200

    Fájl beolvasás/kiírás

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,24 @@
 package com.company;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.String;
+
+
+
+
 
 public class Main {
 
     static List<Bejegyzes> beList = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
+    static Random rnd = new Random();
 
     public static void kettoPerb(){
         System.out.println("Addja meg mennyi új bejegyzést akar: ");
@@ -24,8 +35,33 @@ public class Main {
             beList.add(l);
         }
         System.out.println(beList);
-
     }
+    public static  List<Bejegyzes> beolvas(String fajlNev){
+
+        try {
+            FileReader fr=new FileReader("bejegyzesek.txt", Charset.defaultCharset());
+            BufferedReader br = new BufferedReader(fr);
+
+            String adatok= br.readLine();
+
+            while (adatok!=null){
+                String[] adat=adatok.split(";");
+                Bejegyzes bejegyzes= new Bejegyzes(adat[0],adat[1]);
+                beList.add(bejegyzes);
+                adatok= br.readLine();
+            }
+            br.close();
+            fr.close();
+        }catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+        return beList;
+    }
+
+
+
+
+
     public static void main(String[] args) {
 
         Bejegyzes b1 = new Bejegyzes("Anita", "Lenni vagy nem lenni, ez itt a kérdés");
@@ -35,6 +71,8 @@ public class Main {
         System.out.println(beList);
 
         kettoPerb();
+        beolvas("bejegyzes.txt");
+
 
 
 
