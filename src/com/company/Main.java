cace5c4c86879b1924da78c49a3a64220377d914
Zita <ambrus.zita99@gmail.com>
2021-09-29 15:41:43 +0200

    Lista ketto bejegyzéssel

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,11 @@ public class Main {
     static List<Bejegyzes> beList = new ArrayList<>();
     public static void main(String[] args) {
 
+        Bejegyzes b1 = new Bejegyzes("Anita", "Lenni vagy nem lenni, ez itt a kérdés");
+        Bejegyzes b2 = new Bejegyzes("Andris", "Hol van az agy amikor az kellene?");
+        beList.add(b1);
+        beList.add(b2);
+        System.out.println(beList);
+
     }
 }
